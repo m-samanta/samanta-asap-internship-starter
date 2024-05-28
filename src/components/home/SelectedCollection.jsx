2b84e89a2a11f02152e50bad8fa51c7f083cf76c
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-05-27 21:53:18 -0400

    Selected Collection

--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,46 +1,73 @@
-import React from "react";
-import SelectedItemVideo from "../../assets/selected-collection.mp4";
-import SelectedItemThumbnail from "../../assets/selected-collection-thumbnail.jpg";
-import SelectedItemLogo from "../../assets/selected-collection-logo.avif";
+import React, { useEffect, useState } from "react";
 import VerifiedIcon from "../../assets/verified.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import Skeleton from "../ui/Skeleton";
 
 export default function SelectedCollection() {
+  const [selectedCollectionData, setSelectedCollectionData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchSelectedCollection() {
+      const { data } = await axios.get(
+        "https://remote-internship-api-production.up.railway.app/selectedCollection"
+      );
+      const collectionData = data.data;
+      setSelectedCollectionData(collectionData);
+      setLoading(false);
+    }
+
+    fetchSelectedCollection();
+  }, []);
+
   return (
     <header>
-      <div className="selected-collection">
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          poster={SelectedItemThumbnail}
-          src={SelectedItemVideo}
-          className="selected-collection__bg"
-        />
-        <div className="selected-collection__description">
-          <img
-            src={SelectedItemLogo}
-            alt=""
-            className="selected-collection__logo"
-          />
-          <h1 className="selected-collection__title">
-            Coachella Throwback Merchandise Trunk
-          </h1>
-          <Link to={'/user'} className="selected-collection__author">
-            By Coachella
-            <img
-              src={VerifiedIcon}
-              className="selected-collection__author__verified"
-            />
-          </Link>
-          <div className="selected-collection__details">10 items · 3.4 ETH</div>
-          <Link to={'/collection'} className="selected-collection__button">
-            <div className="green-pulse"></div>
-            View Collection
-          </Link>
+      {loading ? (
+        <div className="selected-collection">
+          <Skeleton width="100%" height="100%" />
         </div>
-      </div>
+      ) : (
+        <div className="selected-collection">
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            poster={selectedCollectionData.thumbnail}
+            src={selectedCollectionData.videoLink}
+            className="selected-collection__bg"
+          />
+          <div className="selected-collection__description">
+            <img
+              src={selectedCollectionData.logo}
+              alt=""
+              className="selected-collection__logo"
+            />
+            <h1 className="selected-collection__title">
+              {selectedCollectionData.title}
+            </h1>
+            <Link
+              to={`/user/${selectedCollectionData.creatorId}`}
+              className="selected-collection__author"
+            >
+              By {selectedCollectionData.creator}
+              <img
+                src={VerifiedIcon}
+                className="selected-collection__author__verified"
+              />
+            </Link>
+            <div className="selected-collection__details">{`${selectedCollectionData.amountOfItems} items · ${selectedCollectionData.floorPrice} ETH`}</div>
+            <Link
+              to={`/collection/${selectedCollectionData.collectionId}`}
+              className="selected-collection__button"
+            >
+              <div className="green-pulse"></div>
+              View Collection
+            </Link>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
