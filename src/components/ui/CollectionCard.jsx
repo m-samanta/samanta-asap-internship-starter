b9612d327f3ad3eb2aa8864d9e8bd3fe52d5ae69
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-05-30 23:10:37 -0400

    Dynamic Collections Page

---
+++ src/components/ui/CollectionCard.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Skeleton from "../ui/Skeleton";
+import { Link } from "react-router-dom";
+
+const CollectionCard = ({ collection, loading }) => {
+  return (
+    <>
+      {loading ? (
+        <div className="collection">
+          <Skeleton width="100%" height="180px" />
+          <div className="collection__info">
+            <h3 className="collection__name"></h3>
+            <div className="collection__stats">
+              <div className="collection__stat">
+                <span className="collection__stat__label">
+                  <Skeleton width="48px" height="16px" borderRadius="4px" />
+                </span>
+                <span className="collection__stat__data">
+                  <Skeleton width="80%" height="16px" borderRadius="4px" />
+                </span>
+              </div>
+              <div className="collection__stat">
+                <span className="collection__stat__label">
+                  <Skeleton width="48px" height="16px" borderRadius="4px" />
+                </span>
+                <span className="collection__stat__data">
+                  <Skeleton width="80%" height="16px" borderRadius="4px" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Link
+          to={`/collection/${collection.collectionId}`}
+          className="collection"
+        >
+          <img
+            src={collection.imageLink}
+            alt={collection.title}
+            className="collection__img"
+          />
+          <div className="collection__info">
+            <h3 className="collection__name">{collection.title}</h3>
+            <div className="collection__stats">
+              <div className="collection__stat">
+                <span className="collection__stat__label">Floor</span>
+                <span className="collection__stat__data">
+                  {parseFloat(collection.floor).toFixed(2)} ETH
+                </span>
+              </div>
+              <div className="collection__stat">
+                <span className="collection__stat__label">Total Volume</span>
+                <span className="collection__stat__data">
+                  {collection.totalVolume} ETH
+                </span>
+              </div>
+            </div>
+          </div>
+        </Link>
+      )}
+    </>
+  );
+};
+
+export default CollectionCard;
--- src/pages/CollectionsPage.jsx
+++ src/pages/CollectionsPage.jsx
@@ -1,45 +1,57 @@
-import React, { useEffect } from "react";
-import SelectedCollection from "../components/home/SelectedCollection";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CollectionCard from "../components/ui/CollectionCard";
 
 export default function CollectionsPage() {
+  const [collectionsPageData, setCollectionsPageData] = useState([]);
+  const [renderCount, setRenderCount] = useState(12);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    async function fetchCollectionsPage() {
+      const { data } = await axios.get(
+        "https://remote-internship-api-production.up.railway.app/collections"
+      );
+      const collectionsPageData = data.data;
+      setCollectionsPageData(collectionsPageData);
+      setLoading(false);
+    }
+
+    fetchCollectionsPage();
     window.scrollTo(0, 0);
   }, []);
+
+  const loadMoreButton = () => {
+    if (loading) return;
+    else setRenderCount((prevCount) => prevCount + 6);
+  };
+
+  const hideLoadMoreButton = () => {
+    if (collectionsPageData.length <= renderCount || renderCount % 6 !== 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   return (
     <div className="container">
       <div className="row">
         <h1 className="collections-page__title">Collections</h1>
         <div className="collections__body">
-          {new Array(12).fill(0).map((_, index) => (
-            <div className="collection-column">
-              <Link to="/collection" key={index} className="collection">
-                <img
-                  src="https://i.seadn.io/gcs/files/a5414557ae405cb6233b4e2e4fa1d9e6.jpg?auto=format&dpr=1&w=1920"
-                  alt=""
-                  className="collection__img"
-                />
-                <div className="collection__info">
-                  <h3 className="collection__name">Bored Ape Kennel Club</h3>
-                  <div className="collection__stats">
-                    <div className="collection__stat">
-                      <span className="collection__stat__label">Floor</span>
-                      <span className="collection__stat__data">0.46 ETH</span>
-                    </div>
-                    <div className="collection__stat">
-                      <span className="collection__stat__label">
-                        Total Volume
-                      </span>
-                      <span className="collection__stat__data">281K ETH</span>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          ))}
+          {collectionsPageData
+            .slice(0, renderCount)
+            .map((collection, index) => (
+              <div className="collection-column" key={index}>
+                <CollectionCard collection={collection} loading={loading} />
+              </div>
+            ))}
         </div>
-        <button className="collections-page__button">Load more</button>
+        {!hideLoadMoreButton() && (
+          <button className="collections-page__button" onClick={loadMoreButton}>
+            Load more
+          </button>
+        )}
       </div>
     </div>
   );
